386bb8af9f047fb288d37f0ce94b58b98a9de4e4
黃啟宸 <william2200123@gmail.com>
2025-05-12 18:10:00 +0800

    新增 HomeController 類別，設定首頁映射及歡迎訊息；新增 index.html 模板以顯示訊息

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.kt
@@ -0,0 +1,15 @@
+package com.example.demo.controller // 確認 package
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/") // 確認映射
+    fun home(model: Model): String {
+        model.addAttribute("mes***REMOVED***ge", "歡迎來到我的網站！")
+        return "index" // 確認視圖名稱
+    }
+}
